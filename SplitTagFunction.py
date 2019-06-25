ff642a71701d8df8ca5fae674b6969b7ba444ccb
Davis <davisfischerwalker@gmail.com>
2019-06-25 11:43:38 -0700

    Create SplitTagFunction.py
    A simple function to split any tags that we have.

---
+++ SplitTagFunction.py
@@ -0,0 +1,14 @@
+#Sample tags to test the function.
+    pass
+
+tags = "world of warcraft, pvp, pve, aliance, hunter, tank, titan, warlock, shadowbringer, destiny, final fantasy, player versus enemy, player versus player, lol, laugh out loud"
+
+#function
+
+def tagsplit ():
+    x = tags.split(",")
+    print(x)
+
+#Calling the function
+
+tagsplit()
