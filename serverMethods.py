abf7b6405aefed64e98ab19db851f7f37851e093
briatayan <brianna.atayan@gmail.com>
2019-06-26 22:39:09 -0700

    added comments to serverMethods.py

--- serverMethods.py
+++ serverMethods.py
@@ -51,25 +51,42 @@ def getData():
 
 ### SERVER FUNCTIONS ###
 
+
+#    adds a server to the JSON file database -- all args need to be strings, returns
+#    True if the item was added, False if it wasn't (this can be used to determine
+#    the response from the bot, and is the same format in the rest of the functions)
+
+
 def serverAdd (serverID, name, tag, description):
     isAdded = False
+    # initializes new item to be inserted into dictionary
     newVal = {serverID : {"name" : name, "tags" : tag, "Description" : description}}
+    # gets dictionary from JSON file, all keys and values are strings
     serverlist = getData()
+    # adds newVal to new server_list file if the file doesn't exist
     if serverlist is None:
         initList(newVal)
         isAdded = True
         return isAdded
+    # returns false if already in serverlist
     if serverID in serverlist:
         return isAdded
     else:
+    # adds newVal to serverlist, updates JSON, and returns True
         serverlist.update(newVal)
         writeData(serverlist)
         isAdded = True
         return isAdded
 
+#    removes a server from the serverlist -- serverID is also a string, returns
+#    True if it was successfully removed, False if not.
+
 def serverRemove(serverID):
     isRemoved = False
+    # gets serverlist from JSON file
     serverlist = getData()
+    # checks if serverID is in the serverlist, if True, then pop serverID off the
+    # dictionary and reutrn True, return False otherwise
     if serverID in  serverlist:
         serverlist.pop(serverID)
         writeData(serverlist)
@@ -78,10 +95,15 @@ def serverRemove(serverID):
     else:
         return isRemoved
 
+#    edits a server when given a key to be edited and its new value -- all
+#    arguments are strings, and returns True if successfully edited, False if not
+
 def serverEdit(serverID, key, newVal):
     isEdited = False
+    # gets serverlist from JSON file
     serverlist = getData()
     if serverID in serverlist:
+        # if the server is found in the list, then set the given key to the new value
         serverlist[serverID][key] = newVal
         writeData(serverlist)
         isEdited = True
